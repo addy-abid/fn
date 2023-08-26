25ccf17a1d05ae789a5e9b8987726f836569debf
AddyAbid <addyabid@Adnans-MacBook-Pro.local>
2023-08-25 19:57:29 -0700

    cors...

--- medusa-config.js
+++ medusa-config.js
@@ -22,11 +22,10 @@ try {
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS;
+const ADMIN_CORS = process.env.ADMIN_CORS;
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS;
+const STORE_CORS = process.env.STORE_CORS || 'http://localhost:8000';
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
