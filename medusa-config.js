0aefe06a8d5dd578f9d08077a6bfc34ba73e5705
Addy <addy.abeed@gmail.com>
2023-08-25 23:52:47 -0700

    install minio to allow file uploads via admin

--- medusa-config.js
+++ medusa-config.js
@@ -50,6 +50,15 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: `medusa-file-minio`,
+    options: {
+        endpoint: process.env.MINIO_ENDPOINT,
+        bucket: process.env.MINIO_BUCKET,
+        access_key_id: process.env.MINIO_ACCESS_KEY,
+        secret_access_key: process.env.MINIO_SECRET_KEY,
+    },
+  },
 ];
 
 const modules = {
