a72c42d5c8958b217756d11eba828e6ade1766fc
Addy <addy.abeed@gmail.com>
2023-08-28 23:01:49 -0700

    remove backend_Url

--- medusa-config.js
+++ medusa-config.js
@@ -40,8 +40,7 @@ const plugins = [
         endpoint: process.env.MINIO_ENDPOINT,
         bucket: process.env.MINIO_BUCKET,
         access_key_id: process.env.MINIO_ACCESS_KEY,
-        secret_access_key: process.env.MINIO_SECRET_KEY,
-        backend_url: "https://fn-backend-production.up.railway.app"
+        secret_access_key: process.env.MINIO_SECRET_KEY
     },
   },
   {
